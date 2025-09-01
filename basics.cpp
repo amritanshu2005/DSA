bd842fb916afe52bf09699a0bc9682d4add157bc
Amritanshu kumar <amritanshuchaudhary60@gmail.com>
2025-09-01 20:56:04 +0530

    MERGE TWO SORTEDARRAY

---
+++ basics.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    cout<<" enter array ele";
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+    }
+        for(int i=0;i<5;i++){
+            cout<<arr[i]<<" ";
+        }
+        int size=8,b[size];
+}
